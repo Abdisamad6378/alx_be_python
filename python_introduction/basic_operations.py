fc53cc694e794b7204f08cd026083871446cd1a7
Abdisamad6378 <abdisamadhussein77@gmail.com>
2025-11-09 23:14:40 +0300

    Add basic operation.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+number1 + number2
+number2 - number1
+number1 * number2
+result = number1 - number2
+operation = "Subtraction"
+print(f"{operation} of {number1} and {number2} is {result}")
