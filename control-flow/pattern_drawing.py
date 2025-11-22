0347bedfefb74779de7a8eb9c0c871db773b188c
Abdisamad6378 <abdisamadhussein77@gmail.com>
2025-11-22 21:46:52 +0300

    add pattern drawing file

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,18 @@
+"""Draw a pattern using nested loops."""
+6
+
+# Prompt user for pattern size
+size = int(input("Enter the size of the pattern: "))
+
+print(f"Creating a {size}x{size} pattern:\n")
+
+row = 0
+while row < size:
+    col = 0
+    while col < size:
+        print("*", end=" ")
+        col += 1
+    print()  # New line after each row
+    row += 1
+
+print(f"\nDone! Created a {size} row by {size} column square.")
--- elseif.py/ifelse.py
+++ elseif.py/ifelse.py
@@ -1,4 +1,1 @@
-"""Print numbers from 0 to 21."""
 
-for number in range(8):
-    print(number*4)
