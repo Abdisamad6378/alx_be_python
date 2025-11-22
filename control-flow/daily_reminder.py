5e27b0328123d3d61787435f97df8d7e3654f0eb
Abdisamad6378 <abdisamadhussein77@gmail.com>
2025-11-22 22:26:23 +0300

    made chnges Daily reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,26 +1,27 @@
 """Daily reminder system that provides task reminders based on priority and time constraints."""
 
-task = input("Enter your task:")
-priority = input("priority (high, medium, low): ").lower()
-time_bound = input("is it time bound (yes or no): " ).lower()
+Task = input("Enter your task:")
+Time_bound = input("is it time bound (yes or no): " ).lower()
+Priority = input("priority (high, medium, low): ").lower()
 
-match priority:
+
+match Priority:
     case "high":
-        if time_bound == "yes":
-            print(f"\nReminder: '{task}' is high priority task "
+        if Time_bound == "yes":
+            print(f"\nReminder: '{Task}' is high priority task "
                   f"it requires immediate attention today!")
         else:
-            print(f"\nReminder: '{task}' is high priority task please adddress it soon")
+            print(f"\nReminder: '{Task}' is high priority task please adddress it soon")
     case "medium":
-        if time_bound == "yes":
-            print(f"\nNote: '{task}' is medium priority task that should be completed today")
+        if Time_bound == "yes":
+            print(f"\nNote: '{Task}' is medium priority task that should be completed today")
         else:
-            print(f"\nNote: '{task}' is medium priority task that should be completed this week ")
+            print(f"\nNote: '{Task}' is medium priority task that should be completed this week ")
     case "low":
-        if time_bound == "low":
-            print(f"\nNote '{task}' is low priority task that should be completed this month")
+        if Time_bound == "low":
+            print(f"\nNote '{Task}' is low priority task that should be completed this month")
         else:
-            print(f"\nNote '{task}' is low priority task "
+            print(f"\nNote '{Task}' is low priority task "
                   f"that should be completed when you have free time")
     case _:
         print("\nInvalid priority level! please use high, medium, low.")
