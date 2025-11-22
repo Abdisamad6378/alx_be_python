1cc0d9fea8116dc6d85e896acd41270f12720446
Abdisamad6378 <abdisamadhussein77@gmail.com>
2025-11-22 22:57:49 +0300

    made chnges Daily reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,24 @@
 """Daily reminder system that provides task reminders based on priority and time constraints."""
 
-Task = input("Enter your task:").lower()
-Time_bound = input("is it time bound (yes or no): " ).lower()
-Priority = input("priority (high, medium, low): ").lower()
+task = input("Enter your task:").lower()
+priority = input("priority (high, medium, low): ").lower()
+time_bound = input("is it time bound (yes or no): " ).lower()
 
 
-match Priority:
+
+match priority:
     case "high":
-        if Time_bound == "yes":
-            print(f"\nReminder: '{Task}' is high priority task "
-                  f"it requires immediate attention today!")
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"\nReminder: '{Task}' is high priority task please adddress it soon")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
     case "medium":
-        if Time_bound == "yes":
-            print(f"\nNote: '{Task}' is medium priority task that should be completed today")
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"\nNote: '{Task}' is medium priority task that should be completed this week ")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
     case "low":
-        if Time_bound == "low":
-            print(f"\nNote '{Task}' is low priority task that should be completed this month")
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"\nNote '{Task}' is low priority task "
-                  f"that should be completed when you have free time")
-    case _:
-        print("\nInvalid priority level! please use high, medium, low.")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
